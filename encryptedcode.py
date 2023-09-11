f79d272a4eaa448c45143ff92f6f345df8b9898a
elizabeth-orton <89154433+elizabeth-orton@users.noreply.github.com>
2023-09-11 13:02:35 -0500

    Add files via upload

---
+++ encryptedcode.py
@@ -0,0 +1,4 @@
+# This program will display an ASCII image of a cat 
+if input("Success! Would you like a cat? (y/n)")=="y":
+    print("|\---/| \n| o_o |\n \_^_/")
+else: ("No cat for you.")
---
+++ packer.py
@@ -0,0 +1,15 @@
+def asciix2(f):
+    encrypted = []
+    encrypted += [2*ord(i) for i in f]
+    return encrypted
+with open('encryptedcode.py') as data:
+    read_data = data.read()
+
+cipher = asciix2(read_data)
+def decryptasciix2(f):
+    decrypted = """"""
+    for i in f:
+        j = int(i/2)
+        decrypted += chr(j)
+    return decrypted
+exec(decryptasciix2(cipher))
