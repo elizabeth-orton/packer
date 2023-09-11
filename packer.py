fe0c64b1c77e03dbb85b83ec506c17b92ce8152f
elizabeth-orton <89154433+elizabeth-orton@users.noreply.github.com>
2023-09-11 13:26:06 -0500

    Update packer.py

--- packer.py
+++ packer.py
@@ -6,10 +6,18 @@ with open('unencryptedcode.py') as data:
     read_data = data.read()
 
 cipher = asciix2(read_data)
+t = open("packedcode.py", "x")
+t.write("""
 def decryptasciix2(f):
-    decrypted = """"""
+    decrypted = ""
     for i in f:
         j = int(i/2)
         decrypted += chr(j)
     return decrypted
-exec(decryptasciix2(cipher))
+""")
+t.close()
+t = open("packedcode.py", "a")
+t.write("cipher = " + str(cipher))
+t.write("\nexec(decryptasciix2(cipher))")
+t.close()
+
