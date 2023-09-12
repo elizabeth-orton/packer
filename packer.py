effafc8e892625a4df177dd934ab895ecf195984
elizabeth-orton <89154433+elizabeth-orton@users.noreply.github.com>
2023-09-12 09:09:21 -0500

    Update packer.py

--- packer.py
+++ packer.py
@@ -1,23 +1,21 @@
-def asciix2(f):
+def asciimultiplied(f, k):
     encrypted = []
-    encrypted += [2*ord(i) for i in f]
+    encrypted += [k*ord(i) for i in f]
     return encrypted
 with open('unencryptedcode.py') as data:
     read_data = data.read()
-
-cipher = asciix2(read_data)
-t = open("packedcode.py", "x")
-t.write("""
-def decryptasciix2(f):
-    decrypted = ""
-    for i in f:
-        j = int(i/2)
-        decrypted += chr(j)
-    return decrypted
-""")
-t.close()
-t = open("packedcode.py", "a")
-t.write("cipher = " + str(cipher))
-t.write("\nexec(decryptasciix2(cipher))")
-t.close()
-
+key = 3
+cipher = asciimultiplied(read_data, key)
+with open("packedcode.py", "x") as t:
+    t.write("""
+def decryptasciix(f, k):
+        decrypted = ""
+        for i in f:
+            j = int(i/k)
+            decrypted += chr(j)
+        return decrypted
+    """)
+with open("packedcode.py", "a") as t:
+    t.write("\ncipher = " + str(cipher))
+    t.write("\nkey =" + str(key))
+    t.write("\nexec(decryptasciix(cipher, key))")
