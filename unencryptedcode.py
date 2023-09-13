46267593c4e67e49ed4e3ed0199c441206be9bce
elizabeth-orton <89154433+elizabeth-orton@users.noreply.github.com>
2023-09-13 12:13:36 -0500

    Update unencryptedcode.py

--- unencryptedcode.py
+++ unencryptedcode.py
@@ -1,4 +1,5 @@
 # This program will display an ASCII image of a cat 
+#this is just for testing purposes, it has nothing to do with the actual packer
 if input("Success! Would you like a cat? (y/n)")=="y":
     print("|\---/| \n| o_o |\n \_^_/")
 else: 
